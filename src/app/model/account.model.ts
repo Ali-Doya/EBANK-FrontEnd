33173508b361a0e88377096de27a469849ce0e10
Ali Doya <ali-doya@hotmail.com>
2022-11-24 14:57:52 +0100

    FrontEnd

---
+++ src/app/model/account.model.ts
@@ -0,0 +1,17 @@
+export interface AccountDetails {
+
+  accountId:            string;
+  balance:              number;
+  currentPage:          number;
+  totalPages:           number;
+  pageSize:             number;
+  accountOperationDTOS: AccountOperation[];
+}
+
+export interface AccountOperation {
+  id:            number;
+  operationDate: Date;
+  amount:        number;
+  description:   string;
+  type:          string;
+}
---
+++ src/app/services/accounts.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AccountDetails} from "../model/account.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  backendHost: string = "http://localhost:8085";
+
+  constructor(private http: HttpClient) {}
+
+
+  public getAccount (accountId : string, page : number, size : number):Observable<AccountDetails>{
+  return this.http.get<AccountDetails>(this.backendHost+"/accounts/"+accountId+"/pageOperations?page="+page+"&size="+size);
+  }
+
+  public debit (accountId : string, amount : number, description : string){
+    let data={accountId : accountId, amount : amount}
+    return this.http.post<AccountDetails>(this.backendHost+"/accounts/debit/",data);
+  }
+
+  public credit (accountId : string, amount : number, description : string){
+    let data={accountId : accountId, amount : amount}
+    return this.http.post<AccountDetails>(this.backendHost+"/accounts/credit/",data);
+  }
+
+  public transfer (accountSource : string, accountDestination : string, amount : number, description : string){
+    let data={accountSource : accountSource, accountDestination : accountDestination, amount : amount}
+    return this.http.post<AccountDetails>(this.backendHost+"/accounts/transfer/",data);
+  }
+}
+
+
